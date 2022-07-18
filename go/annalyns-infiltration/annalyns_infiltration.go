02883fcb79ecd87792660ef9d9885886b882195e
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-07-18 23:32:42 +0800

    [fix]

--- go/annalyns-infiltration/annalyns_infiltration.go
+++ go/annalyns-infiltration/annalyns_infiltration.go
@@ -29,7 +29,8 @@ func CanSignalPrisoner(archerIsAwake, prisonerIsAwake bool) bool {
 // CanFreePrisoner can be executed if the prisoner is awake and the other 2 characters are asleep
 // or if Annalyn's pet dog is with her and the archer is sleeping.
 func CanFreePrisoner(knightIsAwake, archerIsAwake, prisonerIsAwake, petDogIsPresent bool) bool {
-	if (petDogIsPresent && archerIsAwake == false) || (knightIsAwake == false && archerIsAwake == false && prisonerIsAwake == true) {
+	if (petDogIsPresent && archerIsAwake == false) ||
+		(knightIsAwake == false && archerIsAwake == false && prisonerIsAwake == true) {
 		return true
 	} else {
 		return false
