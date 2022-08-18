d74496b7cbd9ba4f246dcf6256d80ea08ab4ddfa
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-08-19 07:21:07 +0800

    [fix]

---
+++ go/animal-magic/animal_magic.go
@@ -0,0 +1,35 @@
+package chance
+
+import (
+	"math/rand"
+	"time"
+)
+
+// SeedWithTime seeds math/rand with the current computer time.
+func SeedWithTime() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RollADie returns a random int d with 1 <= d <= 20.
+func RollADie() int {
+	return rand.Intn(20) + 1
+}
+
+// GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
+func GenerateWandEnergy() float64 {
+	return rand.Float64() + 11.0
+}
+
+// ShuffleAnimals returns a slice with all eight animal strings in random order.
+func ShuffleAnimals() []string {
+	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+
+	sequences := rand.Perm(len(animals))
+	shuffled := make([]string, len(animals))
+
+	for old_index, new_index := range sequences {
+		shuffled[new_index] = animals[old_index]
+	}
+
+	return shuffled
+}
