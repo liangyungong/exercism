6b28bbb98371190a765f3ad4541d3621d0a3b924
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-08-19 10:08:41 +0800

    [fix]

--- go/animal-magic/animal_magic.go
+++ go/animal-magic/animal_magic.go
@@ -17,23 +17,16 @@ func RollADie() int {
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	rand_float := rand.Float64() + float64(rand.Intn(12))
-	if rand_float >= 12.0 {
-		return rand_float - 1.0
-	}
-	return rand_float
+	return rand.Float64() * 12.0
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
 	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
 
-	sequences := rand.Perm(len(animals))
-	shuffled := make([]string, len(animals))
+	rand.Shuffle(len(animals), func(i, j int) {
+		animals[i], animals[j] = animals[j], animals[i]
+	})
 
-	for old_index, new_index := range sequences {
-		shuffled[new_index] = animals[old_index]
-	}
-
-	return shuffled
+	return animals
 }
