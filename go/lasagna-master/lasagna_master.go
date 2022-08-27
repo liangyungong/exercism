8e2d4dbbe1a5f2774f70fb08607c3de3baef95d1
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-08-27 20:42:14 +0800

    [fix]

--- go/lasagna-master/lasagna_master.go
+++ go/lasagna-master/lasagna_master.go
@@ -1,9 +1,43 @@
 package lasagna
 
-// TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, minute_per_layer int) int {
 
-// TODO: define the 'Quantities()' function
+	if minute_per_layer == 0 {
+		minute_per_layer = 2
+	}
 
-// TODO: define the 'AddSecretIngredient()' function
+	return len(layers) * minute_per_layer
+}
 
-// TODO: define the 'ScaleRecipe()' function
+func Quantities(layers []string) (int, float64) {
+	noodle_gram_per_layer := 50
+	source_volume_per_layer := 0.2
+
+	noodle_layer := 0
+	source_layer := 0.0
+
+	for _, layer := range layers {
+		if layer == "noodles" {
+			noodle_layer += 1
+		} else if layer == "sauce" {
+			source_layer += 1.0
+		}
+	}
+
+	return noodle_gram_per_layer * noodle_layer,
+		source_volume_per_layer * source_layer
+}
+
+func AddSecretIngredient(secret_ingredient []string, my_ingredient []string) {
+	my_ingredient[len(my_ingredient)-1] = secret_ingredient[len(secret_ingredient)-1]
+}
+
+func ScaleRecipe(quantities []float64, count int) []float64 {
+	var scaledQuantities []float64 = make([]float64, len(quantities), len(quantities))
+
+	for i, v := range quantities {
+		scaledQuantities[i] = v / 2 * float64(count)
+	}
+
+	return scaledQuantities
+}
