17d673fffb4f97dfe84ca28138291ce6e86c776b
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-09-18 20:34:26 +0800

    [fix]

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -1,5 +1,18 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("Length not equal!")
+	}
+
+	diff := 0
+
+	for idx, _ := range a {
+		if a[idx] != b[idx] {
+			diff++
+		}
+	}
+	return diff, nil
 }
