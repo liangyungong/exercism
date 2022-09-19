06017d2468472c847d2f669a485af6f7d6600af0
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-09-19 20:52:18 +0800

    [fix]

--- go/scrabble-score/scrabble_score.go
+++ go/scrabble-score/scrabble_score.go
@@ -1,5 +1,29 @@
 package scrabble
 
+import "strings"
+
+func lookUpScore(letter string) int {
+	letter = strings.ToLower(letter)
+	score_map := map[string]int{
+		"aeioulnrst": 1,
+		"dg":         2,
+		"bcmp":       3,
+		"fhvwy":      4,
+		"k":          5,
+		"jx":         8,
+		"qz":         10,
+	}
+	for str, score := range score_map {
+		if strings.Contains(str, letter) {
+			return score
+		}
+	}
+	return 0
+}
 func Score(word string) int {
-	panic("Please implement the Score function")
+	total := 0
+	for _, letter := range word {
+		total += lookUpScore(string(letter))
+	}
+	return total
 }
