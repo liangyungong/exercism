63b75f877402e4d9cf42cb68706318f05b333f12
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-09-19 22:56:07 +0800

    [fix]

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -1,13 +1,22 @@
 package diffsquares
 
 func SquareOfSum(n int) int {
-	panic("Please implement the SquareOfSum function")
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+
+	return sum * sum
 }
 
 func SumOfSquares(n int) int {
-	panic("Please implement the SumOfSquares function")
+	sum := 0
+	for i := 1; i <= n; i++ {
+		sum += i * i
+	}
+	return sum
 }
 
 func Difference(n int) int {
-	panic("Please implement the Difference function")
+	return SquareOfSum(n) - SumOfSquares(n)
 }
