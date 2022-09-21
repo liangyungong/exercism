a798e9717588515c52477fcabf2b99e9b55e3bac
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-09-21 23:13:06 +0800

    [fix]

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -1,9 +1,35 @@
 package grains
 
+import (
+	"errors"
+)
+
+const size = 64
+
+func chessBoard() *[size]uint64 {
+	board := [size]uint64{}
+
+	var grid_grain uint64 = 1
+	for i := 0; i < size; i++ {
+		board[i] = grid_grain
+		grid_grain *= 2
+	}
+	return &board
+}
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	board := chessBoard()
+	if number <= 0 || number > 64 {
+		return 0, errors.New("invalid grid number")
+	}
+	return board[number-1], nil
 }
 
 func Total() uint64 {
-	panic("Please implement the Total function")
+	var total uint64 = 0
+	for _, value := range chessBoard() {
+		total += value
+	}
+
+	return total
 }
