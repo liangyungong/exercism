f452553dcce035c10fa9dbf9b36a46ad40d79ddf
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-11-27 00:10:14 +0800

    [fix]

--- clojure/cars-assemble/src/cars_assemble.clj
+++ clojure/cars-assemble/src/cars_assemble.clj
@@ -1,19 +1,22 @@
 (ns cars-assemble)
 
+(defn success-rate
+  [speed]
+  (cond (= speed 0) 0.0
+        (and (>= speed 1) (<= speed 4)) 1.0
+        (and (>= speed 5) (<= speed 8)) 0.9
+        (= speed 9) 0.8
+        (= speed 10) 0.77))
+
 (defn production-rate
   "Returns the assembly line's production rate per hour,
    taking into account its success rate"
   [speed]
   (let [hourly_rate 221]
     (Double/parseDouble (format "%.2f"
-                                (reduce * (vector
-                                           hourly_rate
-                                           (cond (= speed 0) 0.0
-                                                 (and (>= speed 1) (<= speed 4)) 1.0
-                                                 (and (>= speed 5) (<= speed 8)) 0.9
-                                                 (= speed 9) 0.8
-                                                 (= speed 10) 0.77)
-                                           speed))))))
+                                (* (success-rate speed)
+                                   hourly_rate
+                                   speed)))))
 
 (defn working-items
   "Calculates how many working cars are produced per minute"
