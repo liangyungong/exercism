044296093c36a1af01b29123d8bc7d7e40c1f041
Gong LiangYun <18525499+liangyungong@users.noreply.github.com>
2022-11-27 11:44:00 +0800

    [fix]

--- clojure/interest-is-interesting/src/interest_is_interesting.clj
+++ clojure/interest-is-interesting/src/interest_is_interesting.clj
@@ -1,16 +1,26 @@
 (ns interest-is-interesting)
 
 (defn interest-rate
-  "TODO: add docstring"
+  "return the interest rate based balance"
   [balance]
-  )
+  (cond
+    (< balance 0M) -3.213
+    (< balance 1000M) 0.5
+    (< balance 5000M) 1.621
+    :else 2.475))
 
 (defn annual-balance-update
-  "TODO: add docstring"
+  "=> balance + interest"
   [balance]
-  )
+  (let [bigdec-interest (bigdec (interest-rate balance))
+        sign (if (pos? balance) + -)]
+    (sign (* balance (sign (/ bigdec-interest 100M) 1M)))))
 
 (defn amount-to-donate
-  "TODO: add docstring"
+  "=> 2x tax-free"
   [balance tax-free-percentage]
-  )
+  (let [b-tax-free-percentage (bigdec tax-free-percentage)]
+    (let [result (int (/ (* balance b-tax-free-percentage 2) 100))]
+      (if (pos? result)
+        result
+        0))))
